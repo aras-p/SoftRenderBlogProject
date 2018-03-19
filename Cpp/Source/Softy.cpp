6b62deb3d9a79a0a48a73ffeb74dae4ae1a5ab54
Aras Pranckevicius <aras@unity3d.com>
2018-03-19 14:28:12 +0200

    Slightly faster hash for dithering   PC WPF 2.55ms -> 2.41ms   PCNetCore 2.55ms -> 2.53ms   PC Unity 4.80ms -> 4.64ms   MacNetCore 6.40ms -> 6.22ms   PC C++ 1.19ms -> 1.18ms   Mac C++ 4.35ms -> 4.18ms

--- Cpp/Source/Softy.cpp
+++ Cpp/Source/Softy.cpp
@@ -185,12 +185,10 @@ static void UpdateView(RenderObject* obj)
 
 static uint32_t IntHash(uint32_t a)
 {
-    a = (a + 0x7ed55d16) + (a << 12);
-    a = (a ^ 0xc761c23c) ^ (a >> 19);
-    a = (a + 0x165667b1) + (a << 5);
-    a = (a + 0xd3a2646c) ^ (a << 9);
-    a = (a + 0xfd7046c5) + (a << 3);
-    a = (a ^ 0xb55a4f09) ^ (a >> 16);
+    a = a * 2246822519U;
+    a ^= a >> 15;
+    a *= 3266489917U;
+    a ^= a >> 13;
     return a;
 }
 
